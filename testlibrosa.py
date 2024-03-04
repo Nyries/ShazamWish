d7c9270b056bee01d929b66dd53eadd66d386d19
Nyries <cyprien.gaborieau@gmail.com>
2024-03-04 11:32:07 +0100

    testlibrosa3

--- testlibrosa.py
+++ testlibrosa.py
@@ -4,9 +4,12 @@ import librosa
 
 y, sr = librosa.load(librosa.ex('trumpet'))
 S = np.abs(librosa.stft(y))
+chroma = librosa.feature.chroma_stft(S=S, sr=sr)
 
-fig,ax=plt.subplots()
-img = librosa.display.specshow(librosa.amplitude_to_db(S,ref=np.max),y_axis='log',x_axis='time',ax=ax)
-ax.set_title('Power Spectrogram')
-fig.colorbar(img,ax=ax,format="%+2.0f dB")
+fig, ax = plt.subplots(nrows=2, sharex=True)
+img = librosa.display.specshow(librosa.amplitude_to_db(S, ref=np.max),y_axis='log', x_axis='time', ax=ax[0])
+fig.colorbar(img, ax=[ax[0]])
+ax[0].label_outer()
+img = librosa.display.specshow(chroma, y_axis='chroma', x_axis='time', ax=ax[1])
+fig.colorbar(img, ax=[ax[1]])
 plt.show()
