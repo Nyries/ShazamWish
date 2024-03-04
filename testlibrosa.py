99364158cbba0d1f1f1dd9fe4ce79ee95730f246
Nyries <cyprien.gaborieau@gmail.com>
2024-03-04 10:45:25 +0100

    testlibrosa2

--- testlibrosa.py
+++ testlibrosa.py
@@ -1,3 +1,12 @@
 import numpy as np
-import matplotlib as plt
-import librosa as libr
+import matplotlib.pyplot as plt
+import librosa
+
+y, sr = librosa.load(librosa.ex('trumpet'))
+S = np.abs(librosa.stft(y))
+
+fig,ax=plt.subplots()
+img = librosa.display.specshow(librosa.amplitude_to_db(S,ref=np.max),y_axis='log',x_axis='time',ax=ax)
+ax.set_title('Power Spectrogram')
+fig.colorbar(img,ax=ax,format="%+2.0f dB")
+plt.show()
