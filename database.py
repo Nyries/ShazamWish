73ce2a3b8ed90f7dd37360a0ff98bee0291f9e6c
Ambroise012 <ambroise.laroye--langouet@ensea.fr>
2024-03-14 09:35:04 +0100

    update Database

--- database.py
+++ database.py
@@ -2,9 +2,17 @@ import csv
 
 with open('profiles1.csv', 'w', newline='') as file:
     writer = csv.writer(file)
-    field = ["name artisst", "name song", "enveloppe"]
+    field = ["artist name", "song name", "enveloppe"]
+    # l'enveloppe est un array 2D avec les fingerprints
     
     writer.writerow(field)
-    writer.writerow(["Oladele Damilola", "40", "Nigeria"])
-    writer.writerow(["Alina Hricko", "23", "Ukraine"])
-    writer.writerow(["Isabel Walter", "50", "United Kingdom"])
+    writer.writerow(["Bakermat", "You Got Me Where You Want", ""])
+    writer.writerow(["Eminem", "Love Yourself", ""])
+    writer.writerow(["Fred Again", "Delilah", ""])
+    writer.writerow(["Maneskin", "Gasoline", ""])
+    writer.writerow(["Mome", "Aloha", ""])
+    writer.writerow(["Steve Wonder", "Part Time Lover", ""])
+    writer.writerow(["The Rolling Stones", "Satisfaction", ""])
+    writer.writerow(["The Weeknd", "Save Your Tears", ""])
+    writer.writerow(["TRYM", "Falling Star", ""])
+
