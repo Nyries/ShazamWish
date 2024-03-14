f04136e3211eb8b464696d989b0b07f11732ed2b
Luca <136320490+lucacros@users.noreply.github.com>
2024-03-14 08:36:28 +0100

    bouton enter pour stop

--- PlaySong.py
+++ PlaySong.py
@@ -1,13 +1,25 @@
 import librosa
 import numpy as np
 import sounddevice as sd
+import sys
 
 
-filename = "sound/music.mp3"
+def play_sound(filename):
+    y, sr = librosa.load(filename, sr=None)
+    # Start playing the sound
+    sd.play(y, sr)
+
+    # Keep checking for user input to stop the sound
+    print("Press 'Enter' to stop the sound.")
+    input()
+    # Stop the sound
+    sd.stop()
 
 
-y, sr = librosa.load(filename, sr = None)
-sd.play(y,sr)
-sd.wait()
+if __name__ == "__main__":
+    filename = "sound/music.mp3"
+    play_sound(filename)
+
+
 
 
