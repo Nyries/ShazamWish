fdbadb8f07033131f10121e947d74c944b445f3d
Nyries <cyprien.gaborieau@gmail.com>
2024-03-25 10:27:31 +0100

    Comparison Commit

---
+++ Comparison.py
@@ -0,0 +1,62 @@
+from Constellation_map import constellation_map,compute_spectrogram,plot_constellation_map
+import numpy as np
+import matplotlib.pyplot as plt 
+from scipy import ndimage
+
+filename1="songs/Eminem.mp3"
+filename2="songs/Mome.mp3"
+
+def match_binary_matrices_tol(C_ref, C_est, tol_freq=5, tol_time=0):
+    """| Compare binary matrices with tolerance
+    | Note: The tolerance parameters should be smaller than the minimum distance of
+      peaks (1-entries in C_ref ad C_est) to obtain meaningful TP, FN, FP values
+
+    Notebook: C7/C7S1_AudioIdentification.ipynb
+
+    Args:
+        C_ref (np.ndarray): Binary matrix used as reference
+        C_est (np.ndarray): Binary matrix used as estimation
+        tol_freq (int): Tolerance in frequency direction (vertical) (Default value = 0)
+        tol_time (int): Tolerance in time direction (horizontal) (Default value = 0)
+
+    Returns:
+        TP (int): True positives
+        FN (int): False negatives
+        FP (int): False positives
+        C_AND (np.ndarray): Boolean mask of AND of C_ref and C_est (with tolerance)
+    """
+    assert C_ref.shape == C_est.shape, "Dimensions need to agree"
+    N = np.sum(C_ref)
+    M = np.sum(C_est)
+    # Expand C_est with 2D-max-filter using the tolerance parameters
+    C_est_max = ndimage.maximum_filter(C_est, size=(2*tol_freq+1, 2*tol_time+1),
+                                       mode='constant')
+    C_AND = np.logical_and(C_est_max, C_ref)
+    TP = np.sum(C_AND)
+    FN = N - TP
+    FP = M - TP
+    return TP, FN, FP, C_AND
+
+def compare_constellation_maps(fn_wav_D, fn_wav_Q, dist_freq = 11, dist_time = 5, tol_freq = 5, tol_time = 1):
+    Y_D = compute_spectrogram(fn_wav_D)
+    Cmap_D = constellation_map(Y_D, dist_freq, dist_time)
+    Y_Q = compute_spectrogram(fn_wav_Q)
+    Cmap_Q = constellation_map(Y_Q, dist_freq, dist_time)
+
+    TP, FN, FP, Cmap_AND = match_binary_matrices_tol(Cmap_D, Cmap_Q, 
+                                                     tol_freq=tol_freq, tol_time=tol_time)
+    title=r'Matching result (tol_freq=%d and tol_time=%d): TP=%d, FN=%d, FP=%d' % \
+        (tol_freq,tol_time, TP, FN, FP)
+    fig, ax, im = plot_constellation_map(Cmap_AND, color='green', s=200, marker='+', title=title)
+    n, k = np.argwhere(Cmap_D == 1).T
+    ax.scatter(k, n, color='r', s=50, marker='o')
+    n, k = np.argwhere(Cmap_Q == 1).T
+    ax.scatter(k, n, color='cyan', s=20, marker='o')
+    plt.legend(['Matches (TP)', 'Reference', 'Estimation'], loc='upper right', framealpha=1)
+    plt.tight_layout()
+    plt.show()
+
+def is_same_sound(filename1,filename2):
+    
+    return False
+compare_constellation_maps(filename1,filename2)
--- Constellation_map.py
+++ Constellation_map.py
@@ -3,10 +3,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy import ndimage
 
-filename = "songs/Mome.mp3"
+filename = "songs/Eminem.mp3"
 
-def constellation_map(Y,dist_freq=7,dist_time=7,thresh=0.01):
-    """Y: array numpy 2D with the data of chromagram"""
+def constellation_map(Y,dist_freq=15,dist_time=15,thresh=0.05):
+    """Y: array numpy 2D with the data of spectrogram"""
 
     result = ndimage.maximum_filter(Y, size=[2*dist_freq+1, 2*dist_time+1], mode='constant')
     Cmap = np.logical_and(Y == result, result > thresh)
@@ -30,34 +30,27 @@ def plot_constellation_map(Cmap, Y=None, xlim=None, ylim=None, title='',xlabel='
     ax.set_ylabel(ylabel)
     ax.set_title(title)
     Fs = 1
-    if xlim is None:
-        xlim = [-0.5/Fs, (N-0.5)/Fs]
-    if ylim is None:
-        ylim = [-0.5/Fs, (M-0.5)/Fs]
-    #ax.set_xlim(xlim)
-    #ax.set_ylim(ylim)
     n, k = np.argwhere(Cmap == 1).T
     ax.scatter(k, n, color=color, s=s, marker=marker)
     plt.tight_layout()
 
     return fig, ax, im
 
-def compute_spectrogram(audio_file,N=2048,H=1024,Fs=22050,bin_max=128,frame_max=5000):
+def compute_spectrogram(audio_file,N=2048,H=2048,Fs=22050,bin_max=128,frame_max=1000):
     y,fs=librosa.load(audio_file)
     y_duration=len(y)/Fs
-    Y=librosa.stft(y, window='hann')
-    print('a')
+    Y=librosa.stft(y, n_fft=N,hop_length=H,window='hann')
     if bin_max is None:
         bin_max = Y.shape[0]
     if frame_max is None:
-        frame_max = Y.shape[0]
+        frame_max = Y.shape[1]
     Y = np.abs(Y[:bin_max, :frame_max])
 
     return Y
 
-Y=compute_spectrogram(filename)
-Cmap_naive=constellation_map(Y)
+# Y=compute_spectrogram(filename)
+# Cmap_naive=constellation_map(Y)
 
-fig,ax,im=plot_constellation_map(Cmap_naive,Y)
-print(Cmap_naive.shape)
-plt.show()
+# fig,ax,im=plot_constellation_map(Cmap_naive,Y)
+# print(Cmap_naive.shape)
+# plt.show()
